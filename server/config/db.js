211bed69c8a57525db371409bea57dbab2bef3ca
Aravindreddyerri <aravindreddie98@gmail.com>
2022-06-17 14:32:07 -0400

    firstcommit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://aravindreddi:o3AS1NRFQAc8kDXC@cluster0.ldq65g2.mongodb.net/book_store?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
